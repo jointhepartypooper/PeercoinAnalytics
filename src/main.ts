4c68f22b35cd87ab1894723757626f832772e372
jointhepartypooper <counterparty@outlook.com>
2023-12-15 10:55:57 +0100

    https://github.com/vuejs/create-vue + bootstrap 5

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import "./assets/main.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+
+import "./assets/bootstrap-night.min.css";
+
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
